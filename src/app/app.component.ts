6db100fe43564ea22134b656f87039840d9177c6
venkatigitrep <vnempower@gmail.com>
2021-12-12 22:29:31 +0530

    out comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
